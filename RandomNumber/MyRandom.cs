87f7c80d8f9998f8b42c95c5aea59369b17897a1
Andrew Laiacano <alaiacano@visiontech.biz>
2021-04-28 21:22:15 -0400

    inital commit

---
+++ RandomNumber/MyRandom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+
+namespace RandomNumber
+{
+    public class MyRandom
+    {
+
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public MyRandom()
+        {
+        }
+
+        public int GetRand(int min, int max)
+        {
+            Random rand = new Random();
+            return rand.Next(min,max);
+        }
+    }
+}
---
+++ RandomNumber/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("RandomNumber")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("RandomNumber")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
